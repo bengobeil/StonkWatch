9d53b40210983703de73124d3e3da283e0e0662b
Benjamin Gobeil <bkgobeil@gmail.com>
2021-02-23 22:47:34 -0500

    Integrated sutil

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -1,19 +1,63 @@
 module App
 
-open Elmish
-open Elmish.React
+open Sutil
+open Sutil.DOM
+open Sutil.Attr
 
-#if DEBUG
-open Elmish.Debug
-open Elmish.HMR
-#endif
+type Model = { Counter : int }
 
-Program.mkProgram Index.init Index.update Index.view
-#if DEBUG
-|> Program.withConsoleTrace
-#endif
-|> Program.withReactSynchronous "elmish-app"
-#if DEBUG
-|> Program.withDebugger
-#endif
-|> Program.run
+// Model helpers
+let getCounter m = m.Counter
+
+type Message =
+    | Increment
+    | Decrement
+
+let init () : Model = { Counter = 0 }
+
+let update (msg : Message) (model : Model) : Model =
+    match msg with
+    |Increment -> { model with Counter = model.Counter + 1 }
+    |Decrement -> { model with Counter = model.Counter - 1 }
+
+// In Sutil, the view() function is called *once*
+let view() =
+
+    // model is an IStore<ModeL>
+    // This means we can write to it if we want, but when we're adopting
+    // Elmish, we treat it like an IObservable<Model>
+    let model, dispatch = () |> Store.makeElmishSimple init update ignore
+
+    Html.div [
+        // Get used to doing this for components, even though this is a top-level app.
+        disposeOnUnmount [ model ]
+
+        // See Sutil.Styling for more advanced styling options
+        style [
+            Css.fontFamily "Arial, Helvetica, sans-serif"
+            Css.margin 20
+        ]
+
+        // Think of this line as
+        // text $"Counter = {model.counter}"
+        Bind.fragment (model |> Store.map getCounter) <| fun n ->
+            text $"Counter = {n}"
+
+        Html.div [
+            Html.button [
+                class' "button" // Bulma styling, included in index.html
+
+                // Dispatching is as for normal ELmish. Sutil event handlers take an extra options array though
+                onClick (fun _ -> dispatch Decrement) []
+                text "-"
+            ]
+
+            Html.button [
+                class' "button"
+                onClick (fun _ -> dispatch Increment) []
+                text "+"
+            ]
+        ]]
+
+// Start the app
+view() |> mountElement "sutil-app"
--- tests/Client/Client.Tests.fs
+++ tests/Client/Client.Tests.fs
@@ -2,17 +2,9 @@ module Client.Tests
 
 open Fable.Mocha
 
-open Index
-
 let client = testList "Client" [
     testCase "Added todo" <| fun _ ->
-        let newTodo = Todo.create "new todo"
-        let model, _ = init ()
-
-        let model, _ = update (AddedTodo newTodo) model
-
-        Expect.equal 1 model.Todos.Length "There should be 1 todo"
-        Expect.equal newTodo model.Todos.[0] "Todo should equal new todo"
+        ()
 ]
 
 let all =
