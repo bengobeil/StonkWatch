2b9207c0ddc55bc97fc944197fce00369b44a66c
Benjamin Gobeil <bkgobeil@gmail.com>
2021-03-01 11:39:24 -0500

    Update dependencies

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -1,63 +1,52 @@
 module App
 
 open Sutil
+open Sutil.DOM.Html
 open Sutil.DOM
 open Sutil.Attr
 
-type Model = { Counter : int }
-
-// Model helpers
-let getCounter m = m.Counter
+type Model = { Counter: int }
 
 type Message =
-    | Increment
-    | Decrement
+  | Increment
+  | Decrement
 
-let init () : Model = { Counter = 0 }
+let init (): Model = { Counter = 0 }
 
-let update (msg : Message) (model : Model) : Model =
-    match msg with
-    |Increment -> { model with Counter = model.Counter + 1 }
-    |Decrement -> { model with Counter = model.Counter - 1 }
+let update (msg: Message) (model: Model): Model =
+  match msg with
+  | Increment -> { model with Counter = model.Counter + 1 }
+  | Decrement -> { model with Counter = model.Counter - 1 }
 
-// In Sutil, the view() function is called *once*
-let view() =
+let getCounter m = m.Counter
 
-    // model is an IStore<ModeL>
-    // This means we can write to it if we want, but when we're adopting
-    // Elmish, we treat it like an IObservable<Model>
-    let model, dispatch = () |> Store.makeElmishSimple init update ignore
+let view () =
+  // Creating the store "cell" for the whole model, from the update/init functions
+  let model, dispatch = Store.makeElmishSimple init update ignore ()
 
-    Html.div [
-        // Get used to doing this for components, even though this is a top-level app.
-        disposeOnUnmount [ model ]
+  let mkButton textContent msg =
+    button [ class' "button"
+             onClick (fun _ -> dispatch msg) []
+             text textContent ]
 
-        // See Sutil.Styling for more advanced styling options
-        style [
-            Css.fontFamily "Arial, Helvetica, sans-serif"
-            Css.margin 20
-        ]
+  let counterStore = model |> Store.map getCounter
 
-        // Think of this line as
-        // text $"Counter = {model.counter}"
-        Bind.fragment (model |> Store.map getCounter) <| fun n ->
-            text $"Counter = {n}"
+  let counterText =
+    // Bind.fragment takes a store, and a function that takes a real value (non-store, int in this case)
+    // and returns some html
+    Bind.fragment counterStore <| fun counterValue -> text $"Counter: {counterValue}"
+  // Notice, this is the only "Bind" function call, so this is the ONLY part that can change.
 
-        Html.div [
-            Html.button [
-                class' "button" // Bulma styling, included in index.html
+  let buttonsDiv =
+    div [ mkButton "-" Decrement
+          mkButton "+" Increment ]
 
-                // Dispatching is as for normal ELmish. Sutil event handlers take an extra options array though
-                onClick (fun _ -> dispatch Decrement) []
-                text "-"
-            ]
+  div [ disposeOnUnmount [ model ]
+        style [ Css.fontFamily "Arial, Helvetica, sans-serif"
+                Css.margin 20 ]
 
-            Html.button [
-                class' "button"
-                onClick (fun _ -> dispatch Increment) []
-                text "+"
-            ]
-        ]]
+        counterText
+        buttonsDiv ]
 
 // Start the app
-view() |> mountElement "sutil-app"
+view () |> mountElement "sutil-app"
